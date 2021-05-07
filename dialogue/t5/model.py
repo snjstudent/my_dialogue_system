1f5c99cc31e5bc8ee0db2490d98dc2548f7ebbbc
snjstudent <umi.sea.0315@icloud.com>
2021-05-07 18:15:52 +0900

    make test script

--- dialogue/t5/model.py
+++ dialogue/t5/model.py
@@ -9,7 +9,7 @@ import dataloader
 
 
 class T5DialogModel(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, pretrained_path) -> None:
         super().__init__()
         self.t5_model = T5ForConditionalGeneration.from_pretrained(
             "sonoisa/t5-base-japanese")
@@ -26,13 +26,18 @@ class T5DialogModel(nn.Module):
         loss = nn.CrossEntropyLoss(pred, gt)
         return loss
 
+    def generate(self, question):
+        return self.t5_model.generate(input_ids=question["input_ids"],
+                                      attention_mask=question["attention_mask"])
+
 
 class T5DialoguePlModel(pl.LightningModule):
-    def __init__(self, cfg, writer):
+    def __init__(self, cfg, writer, pretrained_path=None):
         super().__init__()
         self.cfg = cfg
         self.writer = writer
-        self.model = T5DialogModel()
+        self.model = T5DialogModel(pretrained_path)
+
         self.tokenizer = T5Tokenizer.from_pretrained(
             "sonoisa/t5-base-japanese")
         self.writer.log_params_from_omegaconf_dict(cfg)
@@ -53,6 +58,14 @@ class T5DialoguePlModel(pl.LightningModule):
         loss_output = outputs[0]
         self.log("validation_loss", loss_output)
         return {"loss": loss_output}
+
+    def test(self, question):
+        self.model.eval()
+        print("Q : " + question)
+        question = self.tokenizer(question, max_length=200, truncation=True,
+                                  padding="max_length", return_tensors="pt")
+        responce = self.model.generate(question)
+        print(self.tokenizer.decode(responce[0]))
 
     def configure_optimizers(self):
         return optim.__dict__[self.cfg.train.optimizer.algorithm](self.model.parameters(), lr=self.cfg.train.optimizer.lr)
--- dialogue/t5/test.py
+++ dialogue/t5/test.py
@@ -6,21 +6,16 @@ import hydra
 from mlflow_utils import MlflowWriter
 import os
 
-EXPERIMENT_NAME = "t5_dialog"
+EXPERIMENT_NAME = "t5_dialog_test"
 
 
 @hydra.main(config_path='config.yaml')
-def train(cfg):
+def test(cfg):
     writer = MlflowWriter(EXPERIMENT_NAME)
     t5_dialogue_model = T5DialoguePlModel.load_from_checkpoint(
         '../../../outputs/2021-05-06/14-14-24/lightning_logs/version_0/checkpoints/epoch=0-step=42558.ckpt', strict=False, cfg=cfg, writer=writer)
     trainer = pl.Trainer(gpus=1, accumulate_grad_batches=8)
-    trainer.fit(t5_dialogue_model)
-    writer.log_artifact(os.path.join(os.getcwd(), '.hydra/config.yaml'))
-    writer.log_artifact(os.path.join(os.getcwd(), '.hydra/hydra.yaml'))
-    writer.log_artifact(os.path.join(os.getcwd(), '.hydra/overrides.yaml'))
-    writer.log_artifact(os.path.join(os.getcwd(), 'main.log'))
-    writer.set_terminated()
+    t5_dialogue_model.test("名前を教えて")
 
 
-train()
+test()
