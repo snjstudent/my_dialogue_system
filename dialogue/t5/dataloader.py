c8a3f8eee4be59e8adfebe2e80ed3331d8eab8d8
shinji <shinji.i@shinjinoMacBook-Pro.local>
2021-05-12 18:39:04 +0900

    create dataset for fine-tuning

--- dialogue/t5/dataloader.py
+++ dialogue/t5/dataloader.py
@@ -1,3 +1,4 @@
+from typing_extensions import ParamSpec
 import torch
 import glob
 import pdb
@@ -61,7 +62,181 @@ class TweetReplyDataSet(torch.utils.data.Dataset):
         return intput_token
 
     def __getitem__(self, idx):
-        
+
+        speak, responce = self.dialog['REQ'][idx], self.dialog['RES'][idx]
+        speak, responce = self.tokenize(speak), self.tokenize(responce)
+        speak, responce = self._reshape_tokens(
+            speak), self._reshape_tokens(responce)
+        # one-hot torch.nn.functional.one_hot(torch.LongTensor(responce), num_classes=50000
+        return speak, responce
+
+
+class MeidaiReplyDataSet(torch.utils.data.Dataset):
+    def __init__(self, mode, do_preprocess: bool = True) -> None:
+        super().__init__()
+        reply_txtfiles = glob.glob(f'../../../meidai/{mode}/*')
+        reply_txtfiles = sorted(reply_txtfiles)
+        self.tokenizer = T5Tokenizer.from_pretrained(
+            "sonoisa/t5-base-japanese")
+        self.do_preprocess = do_preprocess
+        self.dialog = self._sep_req_res(reply_txtfiles)
+
+    def _sep_req_res(self, reply_txtfiles: list) -> dict:
+        dialog_dict = {'REQ': [], 'RES': []}
+        for txt_file in reply_txtfiles:
+            with open(txt_file, "r", errors='ignore') as f:
+                l = f.readlines()
+                for line in l:
+                    try:
+                        speaker, speak = line.split(":", 1)
+                        if self.do_preprocess:
+                            speak = self._preprocess(speak)
+                        dialog_dict[speaker].append(speak)
+                    except:
+                        print("Error : ", line)
+        assert len(dialog_dict['REQ']) == len(dialog_dict['RES']), print(
+            "something wrong with dialogue dataset")
+        return dialog_dict
+
+    @classmethod
+    def _preprocess(self, sentence: str) -> str:
+        sentence = sentence.replace('***', '<UNK>')
+        sentence = sentence.replace('*', '')
+        s = sentence
+        # 参考元：https://qiita.com/gacky01/items/26cd642731e3eddde60d
+        while s.find("（") != -1:
+            start_1 = s.find("（")
+            if s.find("）") != -1:
+                end_1 = s.find("）")
+                if start_1 >= end_1:
+                    s = s.replace(s[end_1], "")
+                else:
+                    s = s.replace(s[start_1:end_1+1], "")
+                if len(s) == 0:
+                    continue
+            else:
+                s = s[0:start_1]
+
+        while s.find("［") != -1:
+            start_2 = s.find("［")
+            if s.find("］") != -1:
+                end_2 = s.find("］")
+                s = s.replace(s[start_2:end_2+1], "")
+            else:
+                s = s[0:start_2]
+
+        while s.find("＜") != -1:
+            start_3 = s.find("＜")
+            if s.find("＞") != -1:
+                end_3 = s.find("＞")
+                s = s.replace(s[start_3:end_3+1], "")
+            else:
+                s = s[0:start_3]
+
+        while s.find("【") != -1:
+            start_4 = s.find("【")
+            if s.find("】") != -1:
+                end_4 = s.find("】")
+                s = s.replace(s[start_4:end_4+1], "")
+            else:
+                s = s[0:start_4]
+        sentence = s
+        return neologdn.normalize(re.sub(r'\d+', '0', sentence)).replace("\n", "")
+
+    def __len__(self):
+        return len(self.dialog['REQ'])
+
+    # トークンの挙動がどう変わるかをチェックしておく
+    def tokenize(self, speak):
+        speak = speak + "</s>"
+        speak = self.tokenizer(speak, max_length=200, truncation=True,
+                               padding="max_length", return_tensors="pt")
+        return speak
+
+    def _reshape_tokens(self, intput_token):
+        intput_token["input_ids"] = intput_token["input_ids"][0]
+        intput_token["attention_mask"] = intput_token["attention_mask"][0]
+        return intput_token
+
+    def __getitem__(self, idx):
+        speak, responce = self.dialog['REQ'][idx], self.dialog['RES'][idx]
+        speak, responce = self.tokenize(speak), self.tokenize(responce)
+        speak, responce = self._reshape_tokens(
+            speak), self._reshape_tokens(responce)
+        # one-hot torch.nn.functional.one_hot(torch.LongTensor(responce), num_classes=50000
+        return speak, responce
+
+
+class J_ToccDataSet(torch.utils.data.Dataset):
+    def __init__(self, mode, do_preprocess: bool = True) -> None:
+        super().__init__()
+        reply_txtfiles = glob.glob(f'../../../j_tocc/{mode}/*')
+        reply_txtfiles = sorted(reply_txtfiles)
+        self.tokenizer = T5Tokenizer.from_pretrained(
+            "sonoisa/t5-base-japanese")
+        self.do_preprocess = do_preprocess
+        self.dialog = self._sep_req_res(reply_txtfiles)
+
+    def _sep_req_res(self, reply_txtfiles: list) -> dict:
+        dialog_dict = {'REQ': [], 'RES': []}
+        for txt_file in reply_txtfiles:
+            with open(txt_file, "r", errors='ignore') as f:
+                l = f.readlines()
+                for line in l:
+                    try:
+                        speaker, speak = line.split(":", 1)
+                        if self.do_preprocess:
+                            speak = self._preprocess(speak)
+                        dialog_dict[speaker].append(speak)
+                    except:
+                        print("Error : ", line)
+        assert len(dialog_dict['REQ']) == len(dialog_dict['RES']), print(
+            "something wrong with dialogue dataset")
+        return dialog_dict
+
+    @classmethod
+    def _preprocess(self, sentence: str) -> str:
+        sentence = sentence.replace('●', '')
+        s = sentence
+        # 参考元：https://qiita.com/gacky01/items/26cd642731e3eddde60d
+        while s.find("（") != -1:
+            start_1 = s.find("（")
+            if s.find("）") != -1:
+                end_1 = s.find("）")
+                if start_1 >= end_1:
+                    s = s.replace(s[end_1], "")
+                else:
+                    s = s.replace(s[start_1:end_1+1], "")
+                if len(s) == 0:
+                    continue
+            else:
+                s = s[0:start_1]
+        while s.find("【") != -1:
+            start_4 = s.find("【")
+            if s.find("】") != -1:
+                end_4 = s.find("】")
+                s = s.replace(s[start_4:end_4+1], "")
+            else:
+                s = s[0:start_4]
+        sentence = s
+        return neologdn.normalize(re.sub(r'\d+', '0', sentence)).replace("\n", "")
+
+    def __len__(self):
+        return len(self.dialog['REQ'])
+
+    # トークンの挙動がどう変わるかをチェックしておく
+    def tokenize(self, speak):
+        speak = speak + "</s>"
+        speak = self.tokenizer(speak, max_length=200, truncation=True,
+                               padding="max_length", return_tensors="pt")
+        return speak
+
+    def _reshape_tokens(self, intput_token):
+        intput_token["input_ids"] = intput_token["input_ids"][0]
+        intput_token["attention_mask"] = intput_token["attention_mask"][0]
+        return intput_token
+
+    def __getitem__(self, idx):
         speak, responce = self.dialog['REQ'][idx], self.dialog['RES'][idx]
         speak, responce = self.tokenize(speak), self.tokenize(responce)
         speak, responce = self._reshape_tokens(
@@ -76,9 +251,17 @@ def get_dataloader(loader_category: str, mode: str, batch_size: int, do_preproce
         TweetReplyDataLoader = torch.utils.data.DataLoader(
             tweetreply_dataset, batch_size=batch_size, shuffle=True)
         return TweetReplyDataLoader
+    elif loader_category == "meidai":
+        meidaireply_dataset = MeidaiReplyDataSet(mode, do_preprocess)
+        MeidaiReplyDataLoader = torch.utils.data.DataLoader(
+            meidaireply_dataset, batch_size=batch_size, shuffle=True)
+        return MeidaiReplyDataLoader
 
-    elif loader_category == "persona":
-        return NotImplementedError
+    elif loader_category == "j-tocc":
+        j_tocc_datataSet = J_ToccDataSet(mode, do_preprocess)
+        J_ToccDataLoader = torch.utils.data.DataLoader(
+            j_tocc_datataSet, batch_size=batch_size, shuffle=True)
+        return J_ToccDataLoader
 
     else:
         print("No matching category!")
